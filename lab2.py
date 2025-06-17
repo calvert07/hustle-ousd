79abf8a3a94de1772cb67c226d30f5f53fccadc8
Calvert Duong <calvertduong@Calverts-MacBook-Air.local>
2025-06-17 11:52:20 -0700

    lab2

---
+++ lab2.py
@@ -0,0 +1,21 @@
+# Lab 2 - Calvert Duong
+
+# Step 1: Name Variable
+name = "Calvert Duong"
+
+# Step 2: Nickname Variable
+nickname = "CD"
+
+# Step 3: Age Variable
+age = 17
+
+# Step 4: Boolean Variable
+used_before = False
+
+# Step 5: List Variable
+hobbies = ["running", "netflix", "sports", "eating"] # List of strings
+
+# Step 6: Dictionary Variable 
+my_favs = {'sports': 'football', 'netflix': 'stranger things', 'running': 'cross country', 'eating': 'ramen'}
+
+# Step 7: Set Variable 
